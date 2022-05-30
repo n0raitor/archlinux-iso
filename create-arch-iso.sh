c1dcc6f76de481dab44cc2f8f1f0cb6bb4cc5eb2
N0Raitor <n0raitor@pm.me>
2022-05-30 14:53:10 +0200

    Rename Scripts as in README.md explained

--- create-arch-iso.sh
+++ create-arch-iso.sh
@@ -2,12 +2,41 @@
 LOCALUSER=norman
 SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 LOCALPATH=/home/norman/GitHub/archlinux-iso/
+done_aur_repo_name="packages_in_repo"
+done_aur_repo_file_exists=0
+create_minimal=0
 
 echo "############################################"
 echo "######## N0Raitor ARCH ISO SCRIPT ##########"
 echo "############################################"
 
 echo ""
+
+# Input validation
+if [ $# -eq 0 ]
+then
+    echo "No Arguments supplied, exiting..."
+    exit 0
+
+else
+    if [ $1 = "--big" ]
+    then
+        echo "Starting \"Big Aur Repo\" Mode"
+        create_minimal=0
+    elif [ $1 = "--minimal" ]
+    then
+        echo "Starting \"Minimal Aur Repo\" Mode"
+        create_minimal=1
+    else
+        echo "Invalid Arguments, exiting ..."
+        exit 0
+    fi
+fi
+if [ -f "$done_aur_repo_name" ]
+then
+    echo "$done_aur_repo_name exists."
+    done_aur_repo_file_exists=1
+fi
 
 echo -n "REInstalling ArchISO... "
 pacman -R --noconfirm archiso  &> /dev/null # Clean Up ArchIsoFiles for a clean build
@@ -20,22 +49,6 @@ if [ -d "$DIR" ]; then
   # Take action if $DIR exists. #
   echo -n "Removing archlive folder... "
   sudo rm -rf ./archlive
-  echo "Done"
-fi
-
-DIR="local/repo"
-if [ -d "$DIR" ]; then
-  # Take action if $DIR exists. #
-  echo -n "Removing local/repo folder... "
-  sudo rm -rf local/repo
-  echo "Done"
-fi
-
-DIR="aur_tmp_build_dir"
-if [ -d "$DIR" ]; then
-  # Take action if $DIR exists. #
-  echo -n "Removing aur_tmp_build_dir folder... "
-  sudo rm -rf aur_tmp_build_dir
   echo "Done"
 fi
 
@@ -53,10 +66,20 @@ echo "Done"
 ### Config Profile (profiledef.sh)
 
 # Set ISO Output Name
-sed -i 's/iso_name="archlinux"/iso_name="archlinux-gnome-n0raitor"/g' ./profiledef.sh
+if [ $create_minimal = 1 ]
+then
+    sed -i 's/iso_name="archlinux"/iso_name="archlinux-gnome-min-n0raitor"/g' ./profiledef.sh
+else
+    sed -i 's/iso_name="archlinux"/iso_name="archlinux-gnome-big-n0raitor"/g' ./profiledef.sh
+fi
 
 # Set ISO Volume Label
-sed -i 's/iso_label="ARCH_$(date +%Y%m)"/iso_label="ARCH_GNOME_N0RAITOR_$(date +%Y%m)"/g' ./profiledef.sh
+if [ $create_minimal = 1 ]
+then
+    sed -i 's/iso_label="ARCH_$(date +%Y%m)"/iso_label="ARCH_GNOME_MIN_N0RAITOR_$(date +%Y%m)"/g' ./profiledef.sh
+else
+    sed -i 's/iso_label="ARCH_$(date +%Y%m)"/iso_label="ARCH_GNOME_BIG_N0RAITOR_$(date +%Y%m)"/g' ./profiledef.sh
+fi
 
 # Set Publisher
 sed -i 's/iso_publisher="Arch Linux <https://archlinux.org>"/iso_publisher="N0Raitor <https://n0raitor.com>"/g' ./profiledef.sh
@@ -64,8 +87,17 @@ sed -i 's/iso_publisher="Arch Linux <https://archlinux.org>"/iso_publisher="N0Ra
 #####################
 # Included Packages #
 #####################
+if [ $create_minimal = 1 ]
+then
+    cat ../additional_packages >> packages.x86_64
+else
+    cat ../additional_packages_big >> packages.x86_64
+fi
 # Add Additional Packages to Iso Build
-cat ../additional_packages >> packages.x86_64
+if [ $done_aur_repo_file_exists = 0 ]
+then
+    cat ../$done_aur_repo_name >> packages.x86_64
+fi
 
 ########################
 # Included Files #######
@@ -73,21 +105,12 @@ cat ../additional_packages >> packages.x86_64
 cd ..
 
 # Include archlinux-installer scripts from N0Raitor
+echo -n "Cloning \"Archlinux-Installer\" by N0Raitor... "
 git clone https://github.com/n0raitor/archlinux-installer.git &> /dev/null
 cp -r archlinux-installer ./archlive/airootfs/root/
 rm -rf archlinux-installer
+echo "Done"
 cd archlive
-
-########################
-# Generate Local Repo ##
-# For AUR Packages on ##
-# ISO                 ##
-########################
-cd .. # Starting from the BASE DIR of this Repository
-sudo -u $LOCALUSER ./create-local-aur-repo.sh
-cd archlive
-cat ../aur_packages >> packages.x86_64  # Add AUR Packages to ISO Included Packages
-
 
 ### Edit Pacman.conf to add local Repository for AUR Packages
 echo "[custom]" >> pacman.conf
