ff40ef4b4b9a6cf91b43680d0437d4d28f5f743b
N0Raitor <n0raitor@pm.me>
2025-02-16 02:05:52 +0100

    Fix

--- create-local-aur-repo.sh
+++ create-local-aur-repo.sh
@@ -27,7 +27,7 @@ else
     then
         echo "Updating AUR Packages"
         sudo ./rm_repo_of_aur.sh
-    elif  [ $2 = "--debug" ]
+    elif [ $2 = "--debug" ]
         echo "Updating AUR Packages"
         sudo ./rm_repo_of_aur.sh
     elif [ $2 != "" ]
